13f95107f45051ba6d6f5c8406d9f44be0938e4d
Ryan Chen <ryan917rc@gmail.com>
2026-01-31 22:34:07 -0500

    reset-project

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col gap-10">
+      <h1 className="text-6xl roboto-mono">Ryan Chen</h1>
+      <h2 className="text-2xl roboto-mono w-1/2 text-center">
+        Helloo! I'm a computer science student at Stony Brook University based
+        in New York City. I specialize in web development with agentic
+        integration.
+      </h2>
+    </div>
+  );
+}
